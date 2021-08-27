b4e354544328b0e7f5986a2a693f764826358f7d
GlibKostsov <glibkostsov@gmail.com>
2021-08-27 15:13:11 +0200

    add HeaderShopListButton animation on addProduct

--- src/components/Layout/HeaderShopListButton.js
+++ src/components/Layout/HeaderShopListButton.js
@@ -1,17 +1,39 @@
-import { useContext } from 'react'
+import { isTemplateElement } from '@babel/types'
+import { useContext, useEffect, useState } from 'react'
+
+import ShopListContext from '../../store/shop-list-context'
 import ShopListIcon from '../ShopList/ShopListIcon'
 import classes from './HeaderShopListButton.module.css'
-import ShopListContext from '../../store/shop-list-context'
 
 const HeaderShopListButton = () => {
+  const [buttonIsBumping, setButtonIsBumping] = useState(false)
+
   const shopListContext = useContext(ShopListContext)
   const { products: productsList } = shopListContext
   const totalProductsAmount = productsList.reduce(
     (total, product) => total + product.amount,
     0
   )
+
+  const buttonClasses = `${classes.button} ${
+    buttonIsBumping ? classes.bump : ''
+  }`
+
+  useEffect(() => {
+    if (productsList.length === 0) {
+      return
+    }
+    setButtonIsBumping(true)
+    const timer = setTimeout(() => {
+      setButtonIsBumping(false)
+    }, 300)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [productsList])
+
   return (
-    <button className={classes.button}>
+    <button className={buttonClasses}>
       <span className={classes.icon}>
         <ShopListIcon />
       </span>
